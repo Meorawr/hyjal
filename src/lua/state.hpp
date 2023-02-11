589d6b0f3f6e96ae5c85738a81af9823f47cc12c
Daniel Yates <me@meorawr.io>
2023-02-11 17:48:30 +0000

    Constrain basic_state template
    Make use of the new pointer_to concept to ensure that the supplied
    pointer-like type has a valid pointer_traits specialization and supports
    the 'std::to_address(p)' machinery.
    
    To cut down on the amount of repetition of repeating the template
    constraints for each member function, the definitions of those members
    are now inlined directly into the basic_state class definition.

--- src/lua/state.hpp
+++ src/lua/state.hpp
@@ -4,6 +4,7 @@
 
 #pragma once
 
+#include "../detail/concepts.hpp"
 #include "stack_algorithms.hpp"
 #include "stack_index.hpp"
 #include "stack_iterator.hpp"
@@ -17,7 +18,7 @@
 #include <utility>
 
 namespace meorawr::hyjal::lua {
-    template<typename Pointer>
+    template<detail::pointer_to<lua_State> Pointer>
     class basic_state {
     public:
         using size_type = index_size_t;
@@ -25,136 +26,97 @@ namespace meorawr::hyjal::lua {
         using iterator = stack_iterator;
         using reverse_iterator = std::reverse_iterator<stack_iterator>;
 
-        constexpr basic_state() noexcept = default;
-        constexpr basic_state(Pointer state) noexcept;
+        constexpr basic_state(Pointer state) noexcept
+            : state_(std::move(state))
+        {
+        }
+
         constexpr basic_state(std::nullptr_t) noexcept = delete;
 
         template<typename... Args>
         requires std::constructible_from<Pointer, Args...>
-        constexpr basic_state(Args&&... args);
+        constexpr basic_state(Args&&... args)
+            : state_(std::forward<Args>(args)...)
+        {
+        }
 
-        constexpr operator lua_State*() const noexcept;
+        constexpr operator lua_State*() const noexcept
+        {
+            return std::to_address(state_);
+        }
 
         // Stack iterators
 
-        iterator begin() const noexcept;
-        stack_top_sentinel end() const noexcept;
-        reverse_iterator rbegin() const noexcept;
-        stack_base_sentinel rend() const noexcept;
+        iterator begin() const noexcept
+        {
+            return iterator(state_, base_index);
+        }
+
+        stack_top_sentinel end() const noexcept
+        {
+            return {};
+        }
+
+        reverse_iterator rbegin() const noexcept
+        {
+            return std::reverse_iterator(iterator(state_, top_index));
+        }
+
+        stack_base_sentinel rend() const noexcept
+        {
+            return {};
+        }
 
         // Stack element accessors
 
-        stack_reference operator[](stack_index index) const noexcept;
-        stack_reference at(stack_index index) const noexcept;
-        stack_reference top() const noexcept;
-        stack_reference base() const noexcept;
+        stack_reference operator[](stack_index index) const noexcept
+        {
+            return stack_reference(state_, index);
+        }
+
+        stack_reference at(stack_index index) const noexcept
+        {
+            return stack_reference(state_, index);
+        }
+
+        stack_reference top() const noexcept
+        {
+            return stack_reference(state_, top_index);
+        }
+
+        stack_reference base() const noexcept
+        {
+            return stack_reference(state_, base_index);
+        }
 
         // Stack capacity
 
-        bool empty() const noexcept;
-        size_type size() const noexcept;
-        constexpr size_type max_size() const noexcept;
+        bool empty() const noexcept
+        {
+            return stack_algorithms::empty(state_);
+        }
+
+        size_type size() const noexcept
+        {
+            return stack_algorithms::size(state_);
+        }
+
+        size_type max_size() const noexcept
+        {
+            return LUAI_MAXCSTACK;
+        }
 
         // Non-member functions
 
-        friend constexpr void swap(basic_state& a, basic_state& b) noexcept;
+        friend constexpr void swap(basic_state& a, basic_state& b) noexcept
+        {
+            using std::swap;
+            swap(a.state_, b.state_);
+        }
 
     private:
         Pointer state_ = {};
     };
-
-    template<typename Pointer>
-    constexpr basic_state<Pointer>::basic_state(Pointer state) noexcept
-        : state_(std::move(state))
-    {
-    }
-
-    template<typename Pointer>
-    template<typename... Args>
-    requires std::constructible_from<Pointer, Args...>
-    constexpr basic_state<Pointer>::basic_state(Args&&... args)
-        : state_(std::forward<Args>(args)...)
-    {
-    }
-
-    template<typename Pointer>
-    constexpr basic_state<Pointer>::operator lua_State*() const noexcept
-    {
-        return std::to_address(state_);
-    }
-
-    template<typename Pointer>
-    inline auto basic_state<Pointer>::begin() const noexcept -> iterator
-    {
-        return iterator(state_, base_index);
-    }
-
-    template<typename Pointer>
-    inline stack_top_sentinel basic_state<Pointer>::end() const noexcept
-    {
-        return {};
-    }
-
-    template<typename Pointer>
-    inline auto basic_state<Pointer>::rbegin() const noexcept -> reverse_iterator
-    {
-        return std::reverse_iterator(iterator(state_, top_index));
-    }
-
-    template<typename Pointer>
-    inline stack_base_sentinel basic_state<Pointer>::rend() const noexcept
-    {
-        return {};
-    }
-
-    template<typename Pointer>
-    inline stack_reference basic_state<Pointer>::operator[](stack_index index) const noexcept
-    {
-        return stack_reference(state_, index);
-    }
-
-    template<typename Pointer>
-    inline stack_reference basic_state<Pointer>::at(stack_index index) const noexcept
-    {
-        return stack_reference(state_, index);
-    }
-
-    template<typename Pointer>
-    inline stack_reference basic_state<Pointer>::top() const noexcept
-    {
-        return stack_reference(state_, top_index);
-    }
-
-    template<typename Pointer>
-    inline stack_reference basic_state<Pointer>::base() const noexcept
-    {
-        return stack_reference(state_, base_index);
-    }
-
-    template<typename Pointer>
-    inline bool basic_state<Pointer>::empty() const noexcept
-    {
-        return stack_algorithms::empty(state_);
-    }
-
-    template<typename Pointer>
-    inline auto basic_state<Pointer>::size() const noexcept -> size_type
-    {
-        return stack_algorithms::size(state_);
-    }
-
-    template<typename Pointer>
-    inline constexpr auto basic_state<Pointer>::max_size() const noexcept -> size_type
-    {
-        return LUAI_MAXCSTACK;
-    }
-
-    template<typename Pointer>
-    constexpr void swap(basic_state<Pointer>& a, basic_state<Pointer>& b) noexcept
-    {
-        using std::swap;
-        swap(a.state_, b.state_);
-    }
 
     using state_view = basic_state<lua_State*>;
     using unique_state = basic_state<std::unique_ptr<lua_State, state_deleter>>;
